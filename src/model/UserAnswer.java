8c95dcf6779b10eac266fe823bbe4012a5693a5e
NightRa <ilan3580@gmail.com>
2013-06-02 00:20:54 +0300

    UserAnswer model.

--- src/model/UserAnswer.java
+++ src/model/UserAnswer.java
@@ -19,27 +19,28 @@ public class UserAnswer implements Model<UserAnswer> {
         this.answer = answer;
     }
 
-    public UserAnswer(String[] params){
-        this(Integer.parseInt(params[0]),Integer.parseInt(params[1]),Integer.parseInt(params[2]),Integer.parseInt(params[3]));
+    public UserAnswer(String[] params) {
+        this(Integer.parseInt(params[0]), Integer.parseInt(params[1]), Integer.parseInt(params[2]), Integer.parseInt(params[3]));
     }
 
-    public UserAnswer(DB db, int userID,int questionID){
+    public UserAnswer(DB db, int userID, int questionID) {
         //Could throw IndexOutOfBounds Exception if no such entry exists.
-        this(db.select("select * from userAnswers where userID="+userID+" and questionID="+questionID+";")[0]);
+        this(db.select("select * from userAnswers where userID=" + userID + " and questionID=" + questionID + ";")[0]);
     }
 
     @Override
     public void save(DB db) {
-
+        db.update("update userAnswers set userID=" + id + ",questionID=" + questionID + ",answer=" + answer + " where id=" + id);
     }
 
     @Override
     public UserAnswer create(DB db) {
-        return null;
+        db.update("insert into userAnswers (userID,questionID,answer) values (" + userID + "," + questionID + "," + answer + ");");
+        return new UserAnswer(db, userID, questionID);
     }
 
     @Override
     public void delete(DB db) {
-
+        db.update("delete from userAnswers where id=" + id);
     }
 }
